c6a4bdc0679f332942d686e65a64dad702bb99ba
Michel Saber <michel-saber@hotmail.com>
2023-04-29 17:07:44 +0200

    login routes and signup completed without middlewares

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -23,11 +23,48 @@ router.post("/signup", async (req, res, next) => {
       profilePhoto: req.body.profilePhoto,
       eventsCreated: req.body.eventsCreated,
     });
-
-    //res.redirect("/profile");
+    res.redirect("/profile");
   } catch (err) {
     console.log("there was an error", err);
-    //res.redirect("/profile");
+    res.redirect("/profile");
+  }
+});
+
+//Route for Login
+router.get("/login", (req, res, next) => {
+  res.render("auth/login");
+});
+
+router.post("/login", async (req, res, next) => {
+  try {
+    const user = await UserModel.findOne({ email: req.body.email });
+
+    console.log(user);
+
+    if (!user) {
+      return res.render("auth/login", { error: "User not existent" });
+    }
+
+    const passwordsMatch = await bcryptjs.compare(
+      req.body.password,
+      user.password
+    );
+
+    if (!passwordsMatch) {
+      return res.render("auth/login", {
+        error: "Sorry the password is incorrect!",
+      });
+    }
+
+    req.session.user = {
+      email: user.email,
+      username: user.username,
+    };
+
+    console.log(req.body);
+    res.redirect("/profile");
+  } catch (err) {
+    next(err);
   }
 });
 
