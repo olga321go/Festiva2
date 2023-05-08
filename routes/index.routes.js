7d4d075c3041169d3ac3a38158330559bdf88209
olgam <olgamartyniuk88@gmail.com>
2023-05-08 09:28:02 +0200

    festivsl created displayed in profile page

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const authRoutes = require("./auth.routes");
 const isLoggedIn = require("../middlewares/isLoggedIn");
 const UserModel = require("../models/User.model");
+const FestivalModel = require("../models/Festival.model");
 
 router.use("/", authRoutes);
 
@@ -15,9 +16,12 @@ router.get("/profile", isLoggedIn, async (req, res) => {
   // const { userID } = req.session.user._id;
   const usersName = req.session.user.username;
   const currentUser = await UserModel.findOne({ username: usersName }).populate("eventsCreated");
+  const usersEvents = await FestivalModel.find();
   // const currentUser = await UserModel.findById(userID).populate("eventsCreated");
   // console.log(req.session.user);
-  res.render("auth/profile", { user: req.session.user }); 
+  // const user = req.session.user;
+  console.log("LOGGED USER", currentUser);
+  res.render("auth/profile", { currentUser, usersEvents }); 
 });
  
 module.exports = router; 
