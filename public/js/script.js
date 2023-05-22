c15ba4351b9b00400d5da65ee64ad2fb16de9d02
olgam <olgamartyniuk88@gmail.com>
2023-05-22 11:24:52 +0200

    adding saveFavorite function

--- public/js/script.js
+++ public/js/script.js
@@ -2,3 +2,13 @@
 document.addEventListener("DOMContentLoaded", () => {
   console.log("Ironhack-M2-Project JS imported successfully!");
 });
+
+async function saveFavorite(element, festivalId) {
+  element.style.color = "rgb(109, 100, 132)";
+  const url = "/favorites";
+  await fetch(url, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ festivalId }),
+  });
+}
