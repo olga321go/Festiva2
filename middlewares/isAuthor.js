047d0960a1791b9ba30c402b146d403db8d438c9
olgam <olgamartyniuk88@gmail.com>
2023-05-22 12:58:59 +0200

    now isAuthor really working

--- middlewares/isAuthor.js
+++ middlewares/isAuthor.js
@@ -1,14 +1,23 @@
+const FestivalModel = require("../models/Festival.model");
 const UserModel = require("../models/User.model");
 
 const isAuthor = async (req, res, next) => {
     const activeUsersName = req.session.user.username;
-    const author = await UserModel.findOne({ username: activeUsersName });
-    if (author == activeUsersName) {
+    const { festivalID } = req.params;
+    const currentFestival = await FestivalModel.findById(festivalID);
+    const authorID = currentFestival.author;
+    const currentAuthor = await UserModel.findById(authorID);
+    console.log("current author", currentAuthor);
+    const currentAuthorUsername = currentAuthor.username;
+
+    if (currentAuthorUsername === activeUsersName) {
       next();
       return;
+    } else {
+        res.send('Sorry, you are not the author of this event thus you cannot edit or delete it.');
     }
   
-    res.send('Sorry, you are not the author of this event thus you cannot edit or delete it.')
+    
   };
   
   module.exports = isAuthor;
